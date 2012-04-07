e2fa9be17fde404cb9c6dd289b56adb64024d6fa
Ken Ohata <ohata@undapt.jp>
2012-04-07 22:19:48 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScientificProto::Application.config.secret_token = 'd90aa70c9cd759e27a246403c7b60d66db5e366ee284b18935bd41d9a2447d6aba92beda7f31417f826f2bcc12f6ea628e631f4688d0ca068d5fd3f82899bd87'
