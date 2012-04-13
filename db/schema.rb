317d85a13d1fb078d207fa7f9fb07546ceb919f8
Ken Ohata <ohata@undapt.jp>
2012-04-13 21:07:15 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120408023036) do
+ActiveRecord::Schema.define(:version => 20120413120457) do
 
   create_table "answers", :force => true do |t|
     t.integer  "problem_id"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(:version => 20120408023036) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "references", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "referable_id"
+    t.string   "referable_type"
+    t.text     "content"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
 end
