c8a5caee35cac18f8f2197b90bca714042c19c11
Ken Ohata <ohata@undapt.jp>
2012-04-21 13:25:48 +0900

    change attr_accessible 'Watch'

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,5 +1,5 @@
 class Watch < ActiveRecord::Base
-  attr_accessible :user_id, :problem_id
+  attr_accessible :problem_id
   belongs_to :watching_user, class_name: "User", foreign_key: :user_id
   belongs_to :watched_problem, class_name: "Problem", foreign_key: :problem_id
 end
