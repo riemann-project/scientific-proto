73e3e3add4a41be0d7a452676a20028edd3cbbf6
Ken Ohata <ohata@undapt.jp>
2012-04-27 11:43:15 +0900

    fix migration file typo

--- db/migrate/20120427023413_create_usefuls.rb
+++ db/migrate/20120427023413_create_usefuls.rb
@@ -1,7 +1,7 @@
 class CreateUsefuls < ActiveRecord::Migration
   def change
     create_table :usefuls do |t|
-      t.intetger :user_id
+      t.integer :user_id
       t.integer :answer_id
 
       t.timestamps
