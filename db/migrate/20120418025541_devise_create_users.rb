9d6216cb34b8ba6766697a267e94224514d2a5fa
Ken Ohata <ohata@undapt.jp>
2012-04-28 09:47:30 +0900

    add columns to User table

--- db/migrate/20120418025541_devise_create_users.rb
+++ db/migrate/20120418025541_devise_create_users.rb
@@ -1,6 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.float :deviation
+      t.integer :account
+      
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
