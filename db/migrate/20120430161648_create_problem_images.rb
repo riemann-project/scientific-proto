d13c35b51e9f9075e9de7837c4f1d7bad5084941
Ken Ohata <ohata@undapt.jp>
2012-05-01 10:02:47 +0900

    add column to ProblemImage

--- db/migrate/20120430161648_create_problem_images.rb
+++ db/migrate/20120430161648_create_problem_images.rb
@@ -2,6 +2,7 @@ class CreateProblemImages < ActiveRecord::Migration
   def change
     create_table :problem_images do |t|
       t.integer :problem_id
+      t.integer :user_id
       t.string :image
 
       t.timestamps
