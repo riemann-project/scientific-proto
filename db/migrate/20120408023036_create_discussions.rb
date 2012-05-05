0105ff71a527343d7d0843431b49fd4e0a48576a
Ken Ohata <ohata@undapt.jp>
2012-05-06 01:54:30 +0900

    add column 'Title' to 'Discussion'

--- db/migrate/20120408023036_create_discussions.rb
+++ db/migrate/20120408023036_create_discussions.rb
@@ -2,6 +2,7 @@ class CreateDiscussions < ActiveRecord::Migration
   def change
     create_table :discussions do |t|
       t.integer :user_id
+      t.string :title
       t.text :content
       t.integer :discussable_id
       t.string :discussable_type
