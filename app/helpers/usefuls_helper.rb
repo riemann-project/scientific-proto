61403224b2a18ee6ba89b4be527ea8e553b4468f
Ken Ohata <ohata@undapt.jp>
2012-05-06 14:05:48 +0900

    fix bug in view_helper

--- app/helpers/usefuls_helper.rb
+++ app/helpers/usefuls_helper.rb
@@ -3,9 +3,9 @@ module UsefulsHelper
     if user_signed_in?
       if Useful.exists?(reference_id: reference.id, user_id: current_user.id)
         u = Useful.find_by_reference_id_and_user_id(reference.id, current_user.id)
-        link_to "Unuseful", problem_reference_useful_path(answer.problem, reference, u), method: :delete, class: "btn"
+        link_to "Unuseful", problem_reference_useful_path(reference.problem, reference, u), method: :delete, class: "btn"
       else
-        link_to "Useful", problem_reference_usefuls_path(answer.problem, reference), method: :post, class: "btn"
+        link_to "Useful", problem_reference_usefuls_path(reference.problem, reference), method: :post, class: "btn"
       end
     else
       "disabled useful button"
