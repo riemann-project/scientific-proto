58163d952ed39dc3fd8dfd3bb1ef1501aa007d2e
Ken Ohata <ohata@undapt.jp>
2012-05-29 18:01:26 +0900

    fix association of Problem and ProblemTree

--- db/migrate/20120407152125_create_problems.rb
+++ db/migrate/20120407152125_create_problems.rb
@@ -2,6 +2,7 @@ class CreateProblems < ActiveRecord::Migration
   def change
     create_table :problems do |t|
       t.text :content
+      t.integer :problem_tree_id
       t.integer :user_id
       t.integer :view
 
